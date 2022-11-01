d84aea624ec2bf244a45dc1e7a6b66ea0c232fad
Namratayarzal1110 <114657289+Namratayarzal1110@users.noreply.github.com>
2022-11-01 12:36:02 +0530

    Create solution basic.java

---
+++ solution basic.java
@@ -0,0 +1,17 @@
+Input (stdin)
+
+
+Hello world
+I am a file
+Read me until end-of-file.
+Your Output (stdout)
+1 Hello world
+2 I am a file
+3 Read me until end-of-file.
+Expected Output
+
+Download
+1 Hello world
+2 I am a file
+3 Read me until end-of-file.
+BlogScoringEnvironmentFAQAbout UsSupportCareersTerms Of ServicePrivacy Policy
